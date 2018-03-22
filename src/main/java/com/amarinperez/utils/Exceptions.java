9866846783722c6bf1ec66f553a553484e3f6524
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-03-22 16:29:40 +0000

    Add utility to transform checked exceptions into unchecked.

---
+++ src/main/java/com/amarinperez/utils/Exceptions.java
@@ -0,0 +1,20 @@
+package com.amarinperez.utils;
+
+import java.util.concurrent.Callable;
+
+public class Exceptions {
+    /**
+     * Runs a statement that might throw a checked exception, transforming the potential exception into unchecked.
+     *
+     * @param statement the statement to call
+     * @param <T>       return type of the statement
+     * @return value returned by the statement
+     */
+    public static <T> T unchecked(Callable<T> statement) {
+        try {
+            return statement.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
