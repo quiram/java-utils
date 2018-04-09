144b63113a972736929d014c8774c0c036239eff
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-09 12:20:18 +0100

    separeted Collections helpers and NullSafety helpers

---
+++ src/main/java/com/amarinperez/utils/Collections.java
@@ -0,0 +1,34 @@
+package com.amarinperez.utils;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+public class Collections {
+    public static <T, R> R map(T o, Function<T, R> mapper) {
+        return o == null ? null : mapper.apply(o);
+    }
+
+    public static <T> String mapToString(T o) {
+        return map(o, T::toString);
+    }
+
+    public static <T, R> List<R> map(List<T> in, Function<T, R> mapper) {
+        return in.stream().map(mapper).collect(toList());
+    }
+
+    public static <T, R> Set<R> map(Set<T> in, Function<T, R> mapper) {
+        return in.stream().map(mapper).collect(toSet());
+    }
+
+    public static <K, V> Map<K, V> toMap(Collection<V> items, Function<V, K> key) {
+        return items.stream().collect(Collectors.toMap(key, identity()));
+    }
+}
