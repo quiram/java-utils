0f31f9c73758afb1db0a4d84579e634c688f45d5
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-09 12:57:01 +0100

    make argument check helpers static

--- src/main/java/com/amarinperez/utils/ArgumentChecks.java
+++ src/main/java/com/amarinperez/utils/ArgumentChecks.java
@@ -5,19 +5,19 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.function.Function;
 
 public class ArgumentChecks {
-    public void ensureNotNegative(int param, String paramName) {
+    static public void ensureNotNegative(int param, String paramName) {
         ensure(param, paramName, p -> p < 0, "not be negative");
     }
 
-    public void ensureGreaterThanZero(int param, String paramName) {
+    static public void ensureGreaterThanZero(int param, String paramName) {
         ensure(param, paramName, p -> p <= 0, "be greater than zero");
     }
 
-    public void ensureNotBlank(String param, final String paramName) {
+    static public void ensureNotBlank(String param, final String paramName) {
         ensure(param, paramName, StringUtils::isBlank, "have a value");
     }
 
-    public <T> void ensure(T param, String paramName, Function<T, Boolean> checker, String message) {
+    static public <T> void ensure(T param, String paramName, Function<T, Boolean> checker, String message) {
         if (checker.apply(param)) {
             throw new IllegalArgumentException(paramName + " must " + message);
         }
