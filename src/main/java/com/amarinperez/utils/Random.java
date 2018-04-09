051c561399e2094b1e2eb3f1593f58e35400f1f7
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-09 17:15:33 +0100

    add random int helper

--- src/main/java/com/amarinperez/utils/Random.java
+++ src/main/java/com/amarinperez/utils/Random.java
@@ -4,4 +4,8 @@ public class Random {
     public static String randomString() {
         return "" + new java.util.Random().nextLong();
     }
+
+    public static int randomInt() {
+        return new java.util.Random().nextInt();
+    }
 }
