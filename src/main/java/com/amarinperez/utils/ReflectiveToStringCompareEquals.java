c31ac2c867d276496318118991933aaf89455c56
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-09 11:59:34 +0100

    expand the simple ReflectiveToString to also cover compare and equals

---
+++ src/main/java/com/amarinperez/utils/ReflectiveToStringCompareEquals.java
@@ -0,0 +1,23 @@
+package com.amarinperez.utils;
+
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+
+abstract public class ReflectiveToStringCompareEquals<T> implements ReflectiveComparable<T> {
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    @Override
+    public boolean equals(Object that) {
+        return reflectionEquals(this, that);
+    }
+
+    @Override
+    public int hashCode() {
+        return reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return reflectionToString(this);
+    }
+}
