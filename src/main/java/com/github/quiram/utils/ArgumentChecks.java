39e628c0281ceb332b189bab6591db4a64fb42db
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-16 12:16:46 +0100

    document all methods in ArgumentChecks as throwing IllegalArgumentException

--- src/main/java/com/github/quiram/utils/ArgumentChecks.java
+++ src/main/java/com/github/quiram/utils/ArgumentChecks.java
@@ -6,23 +6,24 @@ import java.util.Objects;
 import java.util.function.Function;
 
 public class ArgumentChecks {
-    static public void ensureNotNegative(int param, String paramName) {
+
+    static public void ensureNotNegative(int param, String paramName) throws IllegalArgumentException {
         ensure(param, paramName, p -> p < 0, "not be negative");
     }
 
-    static public void ensureGreaterThanZero(int param, String paramName) {
+    static public void ensureGreaterThanZero(int param, String paramName) throws IllegalArgumentException {
         ensure(param, paramName, p -> p <= 0, "be greater than zero");
     }
 
-    static public void ensureNotBlank(String param, final String paramName) {
+    static public void ensureNotBlank(String param, final String paramName) throws IllegalArgumentException {
         ensure(param, paramName, StringUtils::isBlank, "have a value");
     }
 
-    static public <T> void ensureNotNull(T param, String paramName) {
+    static public <T> void ensureNotNull(T param, String paramName) throws IllegalArgumentException {
         ensure(param, paramName, Objects::isNull, "not be null");
     }
 
-    static public <T> void ensure(T param, String paramName, Function<T, Boolean> failCondition, String message) {
+    static public <T> void ensure(T param, String paramName, Function<T, Boolean> failCondition, String message) throws IllegalArgumentException {
         if (failCondition.apply(param)) {
             throw new IllegalArgumentException(paramName + " must " + message);
         }
