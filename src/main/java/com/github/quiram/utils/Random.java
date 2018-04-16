34799ca77ca5b366e115823dacdd002c415e0a19
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-16 12:15:45 +0100

    add some randomDouble helpers

--- src/main/java/com/github/quiram/utils/Random.java
+++ src/main/java/com/github/quiram/utils/Random.java
@@ -1,5 +1,9 @@
 package com.github.quiram.utils;
 
+import static com.github.quiram.utils.ArgumentChecks.ensureGreaterThanZero;
+import static com.github.quiram.utils.ArgumentChecks.ensureNotNegative;
+import static com.github.quiram.utils.Math.pow;
+
 public class Random {
     private static java.util.Random random = new java.util.Random();
 
@@ -13,5 +17,17 @@ public class Random {
 
     public static long randomLong() {
         return random.nextInt();
+    }
+
+    public static double randomDouble() {
+        return random.nextDouble();
+    }
+
+    public static double randomDouble(int bound, int precision) {
+        ensureGreaterThanZero(bound, "bound");
+        ensureNotNegative(precision, "precision");
+        final int precisionFactor = pow(10, precision);
+        final double allDigits = random.nextInt(bound * precisionFactor);
+        return allDigits / precisionFactor;
     }
 }
