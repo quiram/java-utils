88a8a51c9ef1d3f991fa42a3b9e0db79879cff9c
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-23 20:31:36 +0100

    add helpers to merge maps and sets

--- src/main/java/com/github/quiram/utils/Collections.java
+++ src/main/java/com/github/quiram/utils/Collections.java
@@ -1,12 +1,11 @@
 package com.github.quiram.utils;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
@@ -35,4 +34,26 @@ public class Collections {
     public static <K, V1, V2> Map<K, V2> toMap(Collection<V1> items, Function<V1, K> key, Function<V1, V2> value) {
         return items.stream().collect(Collectors.toMap(key, value));
     }
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> merge(BiFunction<V, V, V> merger, Map<K, V> map1, Map<K, V> map2, Map<K, V>... moreMaps) {
+        Map<K, V> finalMap = new HashMap<>();
+        List<Map<K, V>> allMaps = new LinkedList<>();
+        allMaps.add(map1);
+        allMaps.add(map2);
+        allMaps.addAll(asList(moreMaps));
+        allMaps.forEach(m -> m.forEach((i, bd) -> finalMap.merge(i, bd, merger)));
+
+        return finalMap;
+    }
+
+    @SafeVarargs
+    public static <T> Set<T> merge(Set<T> set1, Set<T> set2, Set<T>... moreSets) {
+        final HashSet<T> finalSet = new HashSet<>(set1);
+        finalSet.addAll(set2);
+        asList(moreSets).forEach(finalSet::addAll);
+
+        return finalSet;
+    }
+
 }
