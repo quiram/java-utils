22bed92413775aff61a5b29759fb383fa8f06463
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-09-17 15:20:31 +0100

    add null-safe unbox method for Boolean

--- src/main/java/com/github/quiram/utils/NullSafety.java
+++ src/main/java/com/github/quiram/utils/NullSafety.java
@@ -7,4 +7,8 @@ public class NullSafety {
     public static <T> void setIfNotNull(T item, Consumer<T> setter) {
         Optional.ofNullable(item).ifPresent(setter);
     }
+
+    public static boolean unbox(Boolean b) {
+        return Optional.ofNullable(b).orElse(false);
+    }
 }
