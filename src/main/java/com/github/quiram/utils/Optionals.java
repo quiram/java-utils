6f565fe30b15072f036107abb34661f22ef7dfb1
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2019-01-31 12:32:18 +0000

    method to combine two optionals into an optional pair

--- src/main/java/com/github/quiram/utils/Optionals.java
+++ src/main/java/com/github/quiram/utils/Optionals.java
@@ -1,5 +1,7 @@
 package com.github.quiram.utils;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,5 +11,13 @@ import static java.util.Collections.singletonList;
 public class Optionals {
     public static <T> List<T> toList(Optional<T> o) {
         return o.isPresent() ? singletonList(o.get()) : emptyList();
+    }
+
+    public static <L, R> Optional<Pair<L, R>> combine(Optional<L> o1, Optional<R> o2) {
+        if (o1.isPresent() && o2.isPresent()) {
+            return Optional.of(Pair.of(o1.get(), o2.get()));
+        }
+
+        return Optional.empty();
     }
 }
