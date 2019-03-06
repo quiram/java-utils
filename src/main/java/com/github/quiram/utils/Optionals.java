f3ce184524943455125a8e3abce28522cff95932
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2019-03-06 10:52:15 +0000

    rewrite to functional style

--- src/main/java/com/github/quiram/utils/Optionals.java
+++ src/main/java/com/github/quiram/utils/Optionals.java
@@ -2,15 +2,15 @@ package com.github.quiram.utils;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 
 public class Optionals {
     public static <T> List<T> toList(Optional<T> o) {
-        return o.isPresent() ? singletonList(o.get()) : emptyList();
+        return o.map(Collections::singletonList).orElse(emptyList());
     }
 
     public static <L, R> Optional<Pair<L, R>> combine(Optional<L> o1, Optional<R> o2) {
