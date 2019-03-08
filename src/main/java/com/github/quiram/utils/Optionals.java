fc358a5f5a7f3bbfa9b7fb7c50492fec2fe8a865
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2019-03-08 18:01:40 +0000

    add method to transform Optional to Stream

--- src/main/java/com/github/quiram/utils/Optionals.java
+++ src/main/java/com/github/quiram/utils/Optionals.java
@@ -5,12 +5,17 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 
 public class Optionals {
     public static <T> List<T> toList(Optional<T> o) {
         return o.map(Collections::singletonList).orElse(emptyList());
+    }
+
+    public static <T> Stream<T> toStream(Optional<T> o) {
+        return o.map(Stream::of).orElse(Stream.empty());
     }
 
     public static <L, R> Optional<Pair<L, R>> combine(Optional<L> o1, Optional<R> o2) {
