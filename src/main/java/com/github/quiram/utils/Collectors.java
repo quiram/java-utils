ae11567fa0a6f0a9b6aaa25a6424bcb87798b34a
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2020-02-25 11:42:22 +0000

    create method to collect a stream to a list of pairs

--- src/main/java/com/github/quiram/utils/Collectors.java
+++ src/main/java/com/github/quiram/utils/Collectors.java
@@ -2,8 +2,10 @@ package com.github.quiram.utils;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collector;
+import java.util.stream.IntStream;
 
 import static com.github.quiram.utils.Collections.head;
 import static java.util.stream.Collectors.collectingAndThen;
@@ -30,6 +32,21 @@ public class Collectors {
                         throw new RuntimeException("Exactly two elements expected, but got " + list);
                     }
                     return Pair.of(list.get(0), list.get(1));
+                }
+        );
+    }
+
+    public static <T> Collector<T, ?, List<Pair<T, T>>> toPairs() {
+        return collectingAndThen(
+                toList(),
+                list -> {
+                    if (list.size() % 2 != 0) {
+                        throw new RuntimeException("An even number of elements was expected, but got " + list);
+                    }
+
+                    return IntStream.range(0, list.size() / 2)
+                            .mapToObj(i -> Pair.of(list.get(2 * i), list.get(2 * i + 1)))
+                            .collect(java.util.stream.Collectors.toList());
                 }
         );
     }
--- src/test/java/com/github/quiram/utils/CollectorsTest.java
+++ src/test/java/com/github/quiram/utils/CollectorsTest.java
@@ -5,9 +5,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static com.github.quiram.utils.Collectors.toPair;
+import static com.github.quiram.utils.Collectors.toPairs;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -36,4 +39,35 @@ public class CollectorsTest {
         onBadInput.expect(RuntimeException.class);
         Stream.of(1, 2, 3).collect(toPair());
     }
+
+    @Test
+    public void emptyStreamCanBeCollectedToListOfPairs() {
+        final List<Pair<Object, Object>> list = Stream.empty().collect(toPairs());
+        assertThat(list, is(emptyList()));
+    }
+
+    @Test
+    public void streamWithTwoElementsCanBeCollectedToListOfPairs() {
+        final List<Pair<Integer, Integer>> pairs = Stream.of(1, 2).collect(toPairs());
+        assertThat(pairs.size(), is(1));
+        final Pair<Integer, Integer> pair = pairs.get(0);
+        assertThat(pair.getLeft(), is(1));
+        assertThat(pair.getRight(), is(2));
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void streamWithOneElementFailsToBeCollectedToPairs() {
+        onBadInput.expect(RuntimeException.class);
+        onBadInput.expectMessage("even number");
+        Stream.of(1).collect(toPairs());
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void streamWithThreeElementFailsToBeCollectedToPairs() {
+        onBadInput.expect(RuntimeException.class);
+        Stream.of(1, 2, 3).collect(toPairs());
+    }
+
 }
